29ab359201082efe20fd1782204fe2da5ee62d65
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2025-11-25 11:20:07 -0700

    Eliminar el controlador Clima y mover la lógica de pronóstico del clima a la clase correspondiente; agregar referencia al proyecto MagnoliaDB.

--- MagnoliaWebAPI10/Controllers/ClimaController.cs
+++ MagnoliaWebAPI10/Controllers/ClimaController.cs
@@ -1,11 +1,35 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MagnoliaWebAPI10
+namespace MagnoliaWebAPI10.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClimaController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ClimaController : ControllerBase
+    // localhost:5001/api/Clima/weatherforecast
+    [HttpGet("weatherforecast")]
+    public ActionResult<WeatherForecast[]> Get()
     {
+        var summaries = new[]
+        {
+            "Helado", "Frío intenso", "Frío", "Fresco", "Templado", "Cálido", "Agradable", "Caluroso", "Sofocante", "Abrasador"
+        };
+
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
     }
+}
+
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
--- MagnoliaWebAPI10/Program.cs
+++ MagnoliaWebAPI10/Program.cs
@@ -19,25 +19,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.MapControllers();
 
-var summaries = new[]
-{
-    "Helado", "Frío intenso", "Frío", "Fresco", "Templado", "Cálido", "Agradable", "Caluroso", "Sofocante", "Abrasador"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapGet("/about", () =>
 {
     return "Hola desde mi WebAPI con Net 10";
@@ -46,7 +27,3 @@ app.MapGet("/about", () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
